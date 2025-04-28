1837fe58a30d9a75a3d0a15006f1744ed6832aeb
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2025-04-28 11:59:55 +0200

    SPO2 functional

--- Software/SPRO2/src/main.cpp
+++ Software/SPRO2/src/main.cpp
@@ -38,7 +38,8 @@ void setup()
     Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
-  
+
+  //settings
   byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
@@ -54,9 +55,20 @@ void loop()
   bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
 
   //read the first 100 samples, and determine the signal range
+  readFirstSamples();
+
+  //Continuously taking samples from MAX30102. Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    readSamples();
+  }
+}
+
+void readFirstSamples()
+{
   for (byte i = 0 ; i < bufferLength ; i++)
   {
-    while (hrSensor.available() == false) //do we have new data?
+  while (hrSensor.available() == false) //do we have new data?
       hrSensor.check(); //Check the sensor for new data
 
     redBuffer[i] = hrSensor.getRed();
@@ -68,53 +80,50 @@ void loop()
     Serial.print(F(", ir="));
     Serial.println(irBuffer[i], DEC);
   }
-
   //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-
-  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
-  while (1)
-  {
-    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
-    for (byte i = 25; i < 100; i++)
-    {
-      redBuffer[i - 25] = redBuffer[i];
-      irBuffer[i - 25] = irBuffer[i];
-    }
-
-    //take 25 sets of samples before calculating the heart rate.
-    for (byte i = 75; i < 100; i++)
-    {
-      while (hrSensor.available() == false) //do we have new data?
-        hrSensor.check(); //Check the sensor for new data
-
-      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
-
-      redBuffer[i] = hrSensor.getRed();
-      irBuffer[i] = hrSensor.getIR();
-      hrSensor.nextSample(); //We're finished with this sample so move to next sample
-
-      //send samples and calculation result to terminal program through UART
-      Serial.print(F("red="));
-      Serial.print(redBuffer[i], DEC);
-      Serial.print(F(", ir="));
-      Serial.print(irBuffer[i], DEC);
-
-      Serial.print(F(", HR="));
-      Serial.print(heartRate, DEC);
-
-      Serial.print(F(", HRvalid="));
-      Serial.print(validHeartRate, DEC);
-
-      Serial.print(F(", SPO2="));
-      Serial.print(spo2, DEC);
-
-      Serial.print(F(", SPO2Valid="));
-      Serial.println(validSPO2, DEC);
-    }
-
-    //After gathering 25 new samples recalculate HR and SP02
-    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-  }
 }
 
+void readSamples()
+{
+  //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+  for (byte i = 25; i < 100; i++)
+  {
+    redBuffer[i - 25] = redBuffer[i];
+    irBuffer[i - 25] = irBuffer[i];
+  }
+
+  //take 25 sets of samples before calculating the heart rate.
+  for (byte i = 75; i < 100; i++)
+  {
+    while (hrSensor.available() == false) //do we have new data?
+      hrSensor.check(); //Check the sensor for new data
+
+    digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+    redBuffer[i] = hrSensor.getRed();
+    irBuffer[i] = hrSensor.getIR();
+    hrSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    //send samples and calculation result to terminal program through UART
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.print(irBuffer[i], DEC);
+
+    Serial.print(F(", HR="));
+    Serial.print(heartRate, DEC);
+
+    Serial.print(F(", HRvalid="));
+    Serial.print(validHeartRate, DEC);
+
+    Serial.print(F(", SPO2="));
+    Serial.print(spo2, DEC);
+
+    Serial.print(F(", SPO2Valid="));
+    Serial.println(validSPO2, DEC);
+  }
+
+  //After gathering 25 new samples recalculate HR and SP02
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+}
